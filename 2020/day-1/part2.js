c4c150706623b67ab2c9fb32a7140029972bc8a9
jamie <jamie.blyth@dunelm.com>
2020-12-03 14:09:47 +0000

    2020 day 1 part 2 added

--- 2020/day-1/part2.js
+++ 2020/day-1/part2.js
@@ -3,19 +3,22 @@ import { readFileSync } from 'fs';
 const input = readFileSync(`./input.txt`, 'utf-8');
 const expenseReport = input.split("\n").map(Number);
 
-for(let i = 0; i < expenseReport.length; i++){
-    for(let n=0; n < expenseReport.length; n++){
-        if(expenseReport[i] + expenseReport[i + n] == 2020){
-            console.log("success");
-            console.log(expenseReport[i]);
-            console.log(expenseReport[i + n]);
-            let answer = expenseReport[i] * expenseReport[i + n]
-            console.log(answer)
-            break
-        }
 
+const findAnswer = () => {
+    for(let i = 0; i < expenseReport.length; i++){
+        for(let n=0; n < expenseReport.length; n++){
+            for(let n2=0; n2 < expenseReport.length; n2++){
+                if(expenseReport[i] + expenseReport[i + n] + expenseReport[i + n2] == 2020){
+                    console.log("success");
+                    let answer = expenseReport[i] * expenseReport[i + n] * expenseReport[i + n2]
+                    return answer
+                }
+            }
+        }
     }
 }
+
+console.log(findAnswer())
 
 
 
